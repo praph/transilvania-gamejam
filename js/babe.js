5b21dfd1e1171d242413d68b3699d0c878c91e14
RaduSebastian <g.cont.sebi@gmail.com>
2019-03-17 09:39:46 +0200

    Cross shot

--- js/babe.js
+++ js/babe.js
@@ -11,50 +11,50 @@ class Babe{
         this.enemies4 = [];
         this.enemies5 = [];
     }
-    
+
     /**
      * Create the sprites from the object
      */
     create(){
         var anim1 = this.map.createFromObjects('Enemies', 'babe', { key: 'tile_castle_sprite', frame: 5 });
-    
+
         anim1.forEach(anim1 => {
             const newEnemy = this.Phaser.physics.add.sprite(anim1.x, anim1.y, 'baba');
             this.Phaser.physics.add.collider(newEnemy, this.Phaser.groundLayer);
             this.enemies.push(newEnemy);
-            
+
             anim1.destroy();
-    
+
             this.Phaser.physics.add.collider(player, newEnemy, this.hitPlayer, null, Phaser);
-    
+
             this.shoot(newEnemy, Phaser.physics);
         })
 
         // other npc characters
         var lady = this.map.createFromObjects('Mina', 'lady', { key: 'tile_castle_sprite', frame: 5 });
-    
+
         lady.forEach(anim1 => {
             const newEnemy = this.Phaser.physics.add.sprite(anim1.x, anim1.y, 'lady');
             this.Phaser.physics.add.collider(newEnemy, this.Phaser.groundLayer);
             this.enemies2.push(newEnemy);
-            
+
             anim1.destroy();
-    
+
             this.Phaser.physics.add.collider(player, newEnemy, this.hitPlayer, null, Phaser);
-    
+
             // this.shoot(newEnemy, Phaser.physics);
         })
         var boss = this.map.createFromObjects('Enemies', 'boss', { key: 'tile_castle_sprite', frame: 5 });
-    
+
         boss.forEach(anim1 => {
             const newEnemy = this.Phaser.physics.add.sprite(anim1.x, anim1.y, 'pope');
             this.Phaser.physics.add.collider(newEnemy, this.Phaser.groundLayer);
             this.enemies3.push(newEnemy);
-            
+
             anim1.destroy();
-    
+
             this.Phaser.physics.add.collider(player, newEnemy, this.hitPlayer, null, Phaser);
-    
+
             // this.shoot(newEnemy, Phaser.physics);
         })
 
@@ -70,45 +70,46 @@ class Babe{
         })
     }
 
-    // 
+    //
     destroyBullet(bullet){
         bullet.destroy();
     }
-    
+
     /**
      * create a missile and send it into the wild
      * collision detection with ground layer
      * call it again if enemy is not destroyed yet
-     * @param {*} enemy 
+     * @param {*} enemy
      */
     shoot(enemy){
         if(this.ifOnCamera(enemy.x, enemy.y)){
             const usturoi = this.Phaser.physics.add.sprite(enemy.x, enemy.y, "usturoi");
             usturoi.body.setAllowGravity(false);
-        
+
             if(enemy.x > player.x) {
                 usturoi.body.velocity.x = -250;
             } else {
                 usturoi.body.velocity.x = 250;
             }
-            
+
             this.Phaser.physics.add.collider(usturoi, this.Phaser.groundLayer, this.destroyBullet, null, Phaser);
         }
-    
+
         setTimeout(() => {
             if(enemy.active)
                 this.shoot(enemy, this.Phaser.physics);
         }, Phaser.Math.FloatBetween(1, 4) * 1000)
     }
+
     
     /**
      * calculate if the enemy is on camera
-     * @param {*} enemyX 
+     * @param {*} enemyX
      */
     ifOnCamera(enemyX){
         if(player.body.x - enemyX < 400 && player.body.x - enemyX > -400)
             return 1;
-        
+
         return 0;
     }
 
@@ -119,7 +120,7 @@ class Babe{
         this.enemies.forEach((enemy, index) => {
             if(!enemy.active)
                 return;
-            
+
             if(enemy.x > player.x) {
                 enemySpeed = -60;
                 enemy.anims.play('baba-left', true);
@@ -133,8 +134,8 @@ class Babe{
 
     /**
      * collision between player and enemy
-     * @param {*} player 
-     * @param {*} enemy 
+     * @param {*} player
+     * @param {*} enemy
      */
     hitPlayer(player, enemy)
     {
--- js/npc/monks.js
+++ js/npc/monks.js
@@ -9,16 +9,16 @@ class Monks{
     create(){
         // other npc characters
         var monks = this.map.createFromObjects('Enemies', 'monks', { key: 'tile_castle_sprite', frame: 5 });
-    
+
         monks.forEach(anim1 => {
             const newEnemy = this.Phaser.physics.add.sprite(anim1.x, anim1.y, 'monk');
             this.Phaser.physics.add.collider(newEnemy, this.Phaser.groundLayer);
             this.enemies.push(newEnemy);
-            
+
             anim1.destroy();
-    
+
             this.Phaser.physics.add.collider(player, newEnemy, this.hitPlayer, null, Phaser);
-    
+
             this.shoot(newEnemy, Phaser.physics);
         })
     }
@@ -29,27 +29,41 @@ class Monks{
         })
     }
     shoot(enemy){
-        const usturoi = this.Phaser.physics.add.sprite(enemy.x, enemy.y, "usturoi");
-        usturoi.body.setAllowGravity(false);
-    
-        if(enemy.x > player.x) {
-            usturoi.body.velocity.x = -500;
-        } else {
-            usturoi.body.velocity.x = 500;
+        if(this.ifOnCamera(enemy.x, enemy.y)){
+            const cross = this.Phaser.physics.add.sprite(enemy.x, enemy.y, "cross");
+            cross.body.setAllowGravity(false);
+
+            if(enemy.x > player.x) {
+                cross.body.velocity.x = -250;
+            } else {
+                cross.body.velocity.x = 250;
+            }
+
+            this.Phaser.physics.add.collider(cross, this.Phaser.groundLayer, this.destroyBullet, null, Phaser);
         }
 
-        bullets.push(usturoi);
-    
         setTimeout(() => {
             if(enemy.active)
                 this.shoot(enemy, this.Phaser.physics);
-        }, Phaser.Math.FloatBetween(1, 10) * 1000)
+        }, Phaser.Math.FloatBetween(1, 4) * 1000)
     }
+
+    /**
+     * calculate if the enemy is on camera
+     * @param {*} enemyX
+     */
+    ifOnCamera(enemyX){
+        if(player.body.x - enemyX < 400 && player.body.x - enemyX > -400)
+            return 1;
+
+        return 0;
+    }
+
     animate(){
         this.enemies.forEach((enemy, index) => {
             if(!enemy.active)
                 return;
-            
+
             if(enemy.x > player.x) {
                 enemySpeed = -60;
                 enemy.anims.play('monk-left', true);
