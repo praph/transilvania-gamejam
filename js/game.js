755f3f26d24223a722d3949104152d7c47d9c505
praph <praphweb@gmail.com>
2019-03-17 11:18:19 +0200

    vampire health

--- js/game.js
+++ js/game.js
@@ -34,6 +34,7 @@ var takenDamage = false;
 var lifesModeText;
 var takenDamageText;
 var fpsText;
+var healhBar = [];
 
 // classes
 var babe;
@@ -68,6 +69,7 @@ function preload ()
     this.load.image("background-1", "assets/background/background-1.png");
     this.load.image("background-2", "assets/background/background-2.png");
     this.load.image("background-3", "assets/background/background-3.png");
+    this.load.image("health-vampire", "assets/Vampire_icon-icons.com_75033.png");
 
     this.load.tilemapTiledJSON("map", "assets/map/map800.json");
 }
@@ -170,8 +172,27 @@ function create ()
     babe.create(this, map, player);
     monks.create(this, map, player);
 
-    fpsText = this.add.text(16, 48, 'fps');
+    fpsText = this.add.text(16, 79, 'fps');
     fpsText.setScrollFactor(0);
+
+    var healthBar1 = this.physics.add.sprite(50, 50, 'health-vampire');
+    healthBar1.displayWidth = 50;
+    healthBar1.displayHeight = 50;
+    healthBar1.setScrollFactor(0);
+    healthBar1.body.setAllowGravity(false);
+    healhBar.push(healthBar1);
+    var healthBar2 = this.physics.add.sprite(115, 50, 'health-vampire');
+    healthBar2.displayWidth = 50;
+    healthBar2.displayHeight = 50;
+    healthBar2.setScrollFactor(0);
+    healthBar2.body.setAllowGravity(false);
+    healhBar.push(healthBar2);
+    var healthBar3 = this.physics.add.sprite(180, 50, 'health-vampire');
+    healthBar3.displayWidth = 50;
+    healthBar3.displayHeight = 50;
+    healthBar3.setScrollFactor(0);
+    healthBar3.body.setAllowGravity(false);
+    healhBar.push(healthBar3);
 }
 
 function oFunctie(sprite, health){
