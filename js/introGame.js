f102654060cdce31d0e55af6374537168b522f35
fnitu <fnitu@users.noreply.github.com>
2019-03-20 03:29:45 +0200

    add scale manager to make it responsive
    see: https://labs.phaser.io/100.html?src=src\scalemanager\fit%20running%20game.js

--- js/introGame.js
+++ js/introGame.js
@@ -1,7 +1,12 @@
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    scale: {
+        parent: 'phaser-container',
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 600
+    },
     physics: {
         default: 'arcade',
         arcade: {
